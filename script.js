14e301bf00ce30da2c42e26e280aaea4e1e522ee
alexanderyanthar <alexander.yanthar@outlook.com>
2023-06-22 20:23:47 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 // firebase confing
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import {
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import {
+//   getDatabase,
+//   ref,
+//   onValue,
+//   update
+// } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCt3ZlRjaXDwrkKK_-oKvcVVYlfggCxgoE",
+//   authDomain: "project-2-53d58.firebaseapp.com",
+//   projectId: "project-2-53d58",
+//   storageBucket: "project-2-53d58.appspot.com",
+//   messagingSenderId: "756070251061",
+//   appId: "1:756070251061:web:b44511ad0e75fccb78cf7c",
+// };
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  import {
   getDatabase,
   ref,
   onValue,
@@ -14,22 +35,31 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCt3ZlRjaXDwrkKK_-oKvcVVYlfggCxgoE",
-  authDomain: "project-2-53d58.firebaseapp.com",
-  projectId: "project-2-53d58",
-  storageBucket: "project-2-53d58.appspot.com",
-  messagingSenderId: "756070251061",
-  appId: "1:756070251061:web:b44511ad0e75fccb78cf7c",
+  apiKey: "AIzaSyB_HLTft4InaZ3bGT-a55eX7wGTPiqMk_4",
+  authDomain: "test-novas-db.firebaseapp.com",
+  databaseURL: "https://test-novas-db-default-rtdb.firebaseio.com",
+  projectId: "test-novas-db",
+  storageBucket: "test-novas-db.appspot.com",
+  messagingSenderId: "858827228524",
+  appId: "1:858827228524:web:a0d2906f90b5dbdff7df49"
 };
+
+
+  // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const itemsRef = ref(database);
 
-const mainContainer = document.querySelector(".mainContainer");
-const cartItems = [];
-const ulCartImages = document.querySelector(".ul-cart-images");
+const cartCountRef = ref(database, "/cartCount");
+let cartNumber = document.getElementById(`cart-number`);
+
+let count = 0;
+const mainContainer = document.querySelector('.mainContainer');
+let cartItems = [];
+const ulCartImages = document.querySelector('.ul-cart-images');
 
 mainContainer.addEventListener("click", function (e) {
   updateCart(e, true);
@@ -71,7 +101,6 @@ const displayItems = (arrayOfItems, node) => {
     itemBtn.id = item.id;
     imgItem.src = item.image;
     itemBtn.classList.add("add-item", "fa-solid", "fa-cart-plus");
-    itemBtn.textContent = `add`;
     itemName.textContent = item.name;
     itemPrice.textContent = `$${item.price}`;
     itemPrice.classList.add(`price`);
@@ -116,37 +145,42 @@ onValue(itemsRef, (data) => {
   console.log(allItems);
 });
 
+onValue(cartCountRef, (snapshot) => {
+  count = snapshot.val() || 0;
+  cartNumber.textContent = count.value;
+  console.log(count);
+});
+
+
 function updateCart(e, addToCart) {
   if (e.target.tagName === "BUTTON") {
     const id = e.target.id;
-    const itemId = `item${id}`;
+    const itemId = `item0${id}`;
     const itemRef = ref(database, `/items/${itemId}`);
-    const updateData = { quantity: 1, inCart: addToCart };
     console.log(itemId);
-    let originalItemPrice = itemData.price;
 
     get(itemRef).then((snapshot) => {
       const itemData = snapshot.val();
       if (itemData) {
         let newQuantity = (itemData.quantity || 0) + (addToCart ? 1 : -1);
+        let newTotalItemPrice = newQuantity * itemData.price;
 
-        let totalItemPrice = 0;
-        if (newQuantity > 0) {
-          totalItemPrice = originalItemPrice * newQuantity;
+        if (addToCart) {
+          count ++;
+        } else {
+          count --;
         }
-        if (newQuantity >= 2) {
-          totalItemPrice = originalItemPrice * itemData.quantity;
-          console.log(totalItemPrice);
-        }
+        cartNumber.textContent = count;
 
         const updateData = {
           quantity: newQuantity,
           inCart: newQuantity > 0,
-          price: totalItemPrice,
+          totalItemPrice: newTotalItemPrice,
         };
         update(itemRef, updateData)
           .then(() => {
             console.log(`update`);
+            updateCartCount();
           })
           .catch((er) => {
             console.error(`failed`, er);
@@ -155,6 +189,52 @@ function updateCart(e, addToCart) {
     });
   }
 }
+
+function updateCartCount() {
+  let totalQuantity = 0;
+
+  cartItems.forEach((item) => {
+    totalQuantity += item.quantity || 0;
+  });
+
+  count = totalQuantity;
+  cartNumber.textContent = count;
+
+  update(cartCountRef, { value: count })
+    .then(() => {
+      console.log('cart updated');
+    })
+    .catch((err) => {
+      console.error('failed cart', err);
+    })
+
+  const totalPriceElement = document.createElement("p");
+  totalPriceElement.textContent = `Total Price: $${calculateTotalPrice()}`;
+  ulCartImages.appendChild(totalPriceElement);
+
+  // Check if the cart is empty
+  if (totalQuantity === 0) {
+    emptyText.style.display = "block";
+    purchaseBtn.style.display = "block";
+    totalPriceElement.style.display = 'none';
+  } else {
+    emptyText.style.display = "none";
+    purchaseBtn.style.display = "none";
+    totalPriceElement.style.display = 'block';
+  }
+  
+}
+
+function calculateTotalPrice() {
+  let totalPrice = 0;
+
+  cartItems.forEach((item) => {
+    totalPrice += item.totalItemPrice || 0;
+  });
+
+  return totalPrice;
+}
+
 
 // ulCartImages.addEventListener('click', function(e) {
 //   updateCart(e, false);
@@ -166,8 +246,7 @@ function updateCart(e, addToCart) {
 let cartCheckOut = document.getElementById(`cart-checkout`);
 let cartPage = document.getElementById(`cart-page`);
 let addItem = document.querySelector(`.add-item`);
-let cartNumber = document.getElementById(`cart-number`);
-let count = 0;
+
 // const imgProd1 = document.getElementById(`prod1`);
 // imgProd1.src = "./assets/prod-1.jpg";
 
@@ -185,62 +264,69 @@ cartCheckOut.addEventListener(`click`, function () {
 
 //------- making the counter working in the icon after clicking in the item---------------
 
-mainContainer.addEventListener(`click`, function (event) {
-  // console.log(event.target);
-  count++;
-  cartNumber.textContent = count;
-  // console.log(count);
-  emptyText.style.display = `none`;
-  purchaseBtn.style.display = `none`;
+// mainContainer.addEventListener(`click`, function (event) {
+//   // console.log(event.target);
+//   // count++;
+//   // cartNumber.textContent = count;
+//   // console.log(count);
+//   emptyText.style.display = `none`;
+//   purchaseBtn.style.display = `none`;
 
-  // if (event.target.itemBtn === `BUTTON`) {
-  //   id = event.target.id;
-  //   console.log(id);
-  // }
+//   // if (event.target.itemBtn === `BUTTON`) {
+//   //   id = event.target.id;
+//   //   console.log(id);
+//   // }
 
-  //   display image in the cart page
-});
+//   //   display image in the cart page
+// });
 
 // ------------------End with counter in the icon --------------------------------
 
 // Display item in cart function
 
+
+
+
 const displayCartItems = (arrOfItems, node) => {
   node.innerHTML = "";
+
 
   arrOfItems.forEach((item) => {
     const liItem = document.createElement(`li`);
     const imgItem = document.createElement(`img`);
     const itemName = document.createElement(`p`);
-    const itemPrice = document.createElement(`p`);
+    const totalItemPrice = document.createElement(`p`);
     const removeBtn = document.createElement(`button`);
     const quantity = document.createElement("span");
 
     itemName.textContent = item.name;
-    itemPrice.textContent = `$${item.price}`;
-    itemPrice.classList.add("itemPrice");
+    totalItemPrice.textContent = `$${item.totalItemPrice}`;
+    totalItemPrice.classList.add("itemPrice");
+
 
     imgItem.src = item.image;
     removeBtn.id = item.id;
     imgItem.alt = item.name;
     removeBtn.classList.add("fa-solid", "fa-cart-arrow-down");
 
+    liItem.classList.add(`liItem`);
+    imgItem.classList.add(`imgItem`);
+    itemName.classList.add(`itemName`);
+    totalItemPrice.classList.add(`itemPrice`);
+    removeBtn.classList.add(`removeBtn`);
+    quantity.classList.add(`quantity`);
+
     quantity.textContent = item.quantity || 0;
     liItem.append(imgItem);
     liItem.append(quantity);
     liItem.append(itemName);
-    liItem.append(itemPrice);
+    liItem.append(totalItemPrice);
     liItem.append(removeBtn);
 
     node.append(liItem);
 
     // adding classes
 
-    liItem.classList.add(`liItem`);
-    imgItem.classList.add(`imgItem`);
-    itemName.classList.add(`itemName`);
-    itemPrice.classList.add(`itemPrice`);
-    removeBtn.classList.add(`removeBtn`);
-    quantity.classList.add(`quantity`);
   });
 };
+
